0ae490dd1cbba0d7011109d6825bf101e4db7d86
RxLam <56826690+RxLam@users.noreply.github.com>
2019-11-08 16:28:15 +0300

    Update index.js

--- src/components/products/item/index.js
+++ src/components/products/item/index.js
@@ -1,1 +1,30 @@
-import React from 'react';import {Link} from 'react-router-dom';import {Button} from 'react-bootstrap'export default function(props){	let btn;	if(props.inCart){		btn =	<Button variant="danger" onClick={props.onRemove}>					Remove				</Button>	} else {		btn = 	<Button variant="success" onClick={props.onAdd}>					Add to cart				</Button> 			}	return(	       <div>	       		<h1>{props.title}</h1>	       		<hr/>	       		<div>	       			<strong>Price: {props.price}</strong>	       		</div>	       		<Link to={props.backUrl}>Back</Link>	       		<p>Text about product</p>	       		{btn}	       </div>				)}
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Button} from 'react-bootstrap'
+
+export default function(props){
+	let btn;
+
+	if(props.inCart){
+		btn =	<Button variant="danger" onClick={props.onRemove}>
+					Remove
+				</Button>
+	} else {
+		btn = 	<Button variant="success" onClick={props.onAdd}>
+					Add to cart
+				</Button> 
+			}
+
+	return(
+	       <div>
+	       		<h1>{props.title}</h1>
+	       		<hr/>
+	       		<div>
+	       			<strong>Price: {props.price}</strong>
+	       		</div>
+	       		<Link to={props.backUrl}>Back</Link>
+	       		<p>Text about product</p>
+	       		{btn}
+	       </div>			
+	)
+};
