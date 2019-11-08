28568ab143fde97627d65bbe45f76f46aeaa9042
RxLam <56826690+RxLam@users.noreply.github.com>
2019-11-08 16:20:50 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-import React from 'react';import ReactDom from 'react-dom';import {Provider} from 'mobx-react';import App from './app';import 'bootstrap/dist/css/bootstrap.min.css';import stores from '~s';stores.products.load().then(() => {	ReactDom.render(<Provider stores={stores}>	<App/>	</Provider>, document.querySelector('#app'));	});stores.cart.load();
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Provider} from 'mobx-react';
+import App from './app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import stores from '~s';
+
+
+stores.products.load().then(() => {
+	ReactDom.render(<Provider stores={stores}>
+	<App/>
+	</Provider>, document.querySelector('#app'));	
+})
+
+stores.cart.load();
+
